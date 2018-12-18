b40b9fdee84029e7c34e8b8cb1dc7fc63e37a96e
toywang <toy_king@163.com>
2018-12-18 12:30:04 +0800

    python test

--- test/test_cold.bash
+++ test/test_cold.bash
@@ -7,6 +7,8 @@ for (( i = 0; i < MAX ; i ++ ))
 do
     echo ${i}
     time curl -H "Host: helloworld-python.default.example.com" http://119.28.33.55
+
+    time curl -H "Host: blue-green-demo.default.example.com" http://119.28.33.55
 done
 
 
